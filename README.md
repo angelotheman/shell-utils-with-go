a275945b785c7d624c6ece3759582229708d963a
Kwabena Twumasi <ktwum721@gmail.com>
2025-10-27 17:48:27 +0000

    Cat command done

---
+++ README.md
@@ -0,0 +1,3 @@
+# Implementing CLI Utils using Go Language
+
+This was a really fun project to help anyone learn Go
--- gocat/main.go
+++ gocat/main.go
@@ -1,20 +1,56 @@
 package main
 
 import (
+	"bufio"
+	"flag"
 	"fmt"
 	"os"
 )
 
 func main() {
-	// Print the arguments passed to the program
-	args := os.Args
 
-	fmt.Println(args)
+	nFlag := flag.Bool("n", false, "number all output lines")
+	EFlag := flag.Bool("E", false, "display $ at end of each line")
 
-	fc, err := os.ReadFile(args[1])
-	if err != nil {
-		fmt.Printf("Error reading file: %v\n", err)
+	flag.Parse()
+
+	files := flag.Args()
+	
+	if len(files) == 0 {
+		fmt.Println("Usage: go run cat.go [-n] [-E] <filename>")
+		return
 	}
 
-	fmt.Println(string(fc))
+	// Looping over each file provided as argument
+
+	for _, filename := range files {
+		file, err := os.Open(filename)
+		if err != nil {
+			fmt.Printf("cat: %s: %v\n", filename, err)
+			continue
+		}
+		defer file.Close()
+
+		scanner := bufio.NewScanner(file)
+		lineNum := 1
+
+		for scanner.Scan() {
+			line := scanner.Text()
+
+			if *EFlag {
+				line += "$"
+			}
+
+			if *nFlag {
+				fmt.Printf("%6d  %s\n", lineNum, line)
+				lineNum++
+			} else {
+				fmt.Println(line)
+			}
+		}
+
+		if err := scanner.Err(); err != nil {
+			fmt.Printf("Error reading file %s: %v\n", filename, err)
+		}
+	}
 }
